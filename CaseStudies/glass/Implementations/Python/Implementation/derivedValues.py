63fee225c72db5ff0e73639eec56fab12bb457c2
Sam Crawford <sam.crawford82@gmail.com>
2018-06-04 10:41:41 -0400

    Closes #67

--- CaseStudies/glass/Implementations/Python/Implementation/derivedValues.py
+++ CaseStudies/glass/Implementations/Python/Implementation/derivedValues.py
@@ -6,7 +6,6 @@ Service: Defines the equations transforming the initial inputs into derived quan
 
 import math
 
-
 def derived_params(params):
     # cal aspect ratio
     params.asprat = params.a / params.b
@@ -22,28 +21,28 @@ def derived_params(params):
         params.h = 2.16
     elif params.t == "2.7":
         params.h = 2.59
-    elif params.t == "3.0":
+    elif (params.t == "3.0"):
         params.h = 2.92
-    elif params.t == "4.0":
+    elif (params.t == "4.0"):
         params.h = 3.78
-    elif params.t == "5.0":
+    elif (params.t == "5.0"):
         params.h = 4.57
-    elif params.t == "6.0":
+    elif (params.t == "6.0"):
         params.h = 5.56
-    elif params.t == "8.0":
+    elif (params.t == "8.0"):
         params.h = 7.42
-    elif params.t == "10.0":
+    elif (params.t == "10.0"):
         params.h = 9.02
-    elif params.t == "12.0":
+    elif (params.t == "12.0"):
         params.h = 11.91
-    elif params.t == "16.0":
+    elif (params.t == "16.0"):
         params.h = 15.09
-    elif params.t == "19.0":
+    elif (params.t == "19.0"):
         params.h = 18.26
-    elif params.t == "22.0":
+    elif (params.t == "22.0"):
         params.h = 21.44
     else:
-        print("Wrong thickness entered -Not a industrial standard ")
+        print("Wrong thickness entered - Not an industrial standard")
 
     # cal glass type factor
     if params.gt == 'AN' or params.gt == 'an':
@@ -54,5 +53,3 @@ def derived_params(params):
         params.gtf = 4
     else:
         print("Wrong glass type entered")
-    return params
-
