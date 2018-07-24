db1d3282b3a10309a24e2fb2cf07c52bd19be313
Sam Crawford <sam.crawford82@gmail.com>
2018-07-24 16:28:24 -0400

    Implemented Constants Module in GlassBR as per #134

---
+++ CaseStudies/glass/src/Python/NewImplementation/Constants.py
@@ -0,0 +1,16 @@
+## @file Constants.py
+#  @brief Library module that exports required constants.
+#  @date 07/24/2018
+
+m     = 7
+k     = 2.86e-53
+E     = 7.17e7
+td    = 3
+LSF   = 1
+dmax  = 5.0
+dmin  = 0.1
+ARmax = 5.0
+wmax  = 910.0
+wmin  = 4.5
+SDmin = 6.0
+SDmax = 130.0
