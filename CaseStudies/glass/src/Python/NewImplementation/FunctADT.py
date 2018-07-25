377fd49b8d8f90154484efb6a75fecfcf8bccbd8
elwazana <elwazana@mcmaster.ca>
2018-07-25 17:39:23 -0400

    W.I.P. Implemented Funct ADT Module as per issue #134

---
+++ CaseStudies/glass/src/Python/NewImplementation/FunctADT.py
@@ -0,0 +1,57 @@
+## @file FunctADT.py
+#  @brief Implements an ADT for a function
+#  @date 07/25/2018
+
+import pytest
+from SeqServices import *
+
+## @brief An implementation for a CurveADT
+class FunctT :
+
+    # exported constants
+    MAX_ORDER = 2
+
+    def __init__ ( self , X , Y , i ):
+
+        # raise any necessary exceptions
+        if (not(isAscending(X))):
+            raise IndepVarNotAscending("Independent variables are not in ascending order!")
+        if (len(X) != len(Y)):
+            raise SeqSizeMismatch("Sequences are not of the same length!")
+        if (not(i in [1, self.MAX_ORDER])):
+            raise InvalidInterpOrder("Invalid interpretation error!")
+        if (len(X) < 3):
+            raise TooFewDataPts("Sequence has too few data points")
+
+        # state variables
+        self.__X = X
+        self.__Y = Y
+        self.__minx = X[0]
+        self.__maxx = X[len(X) - 1]
+        self.__o = i
+
+    ## @brief Gets the minimum independent variable value
+    #  @return smallest x-value in CurveT instance
+    def minD ( self ) :
+        return self.__minx
+
+    ## @brief Gets the maximum independent variable value
+    #  @return largest x-value in CurveT instance
+    def maxD ( self ) :
+        return self.__maxx
+
+    ## @brief Gets the order of the CurveT instance
+    #  @return order of CurveT instance
+    def order ( self ) :
+        return self.__o
+
+    def eval ( self, x ):
+        if (not(self.__minx <= x <= self.__maxx)):
+            raise OutOfDomain("Out of domain!")
+        if (not(1 <= index(self.__X, x) <= (len(self.__X) - 2))):
+            raise OutOfDomain("Out of range!")
+
+        if self.__o == 1:
+            return (interpLin (self.__X, self.__Y, self.__X + 1, self.__Y + 1, x))
+        elif self.__o == 2:
+            return (interpQuad (self.__X - 1, self.__Y - 1, self.__X, self.__Y, self.__X + 1, self.__Y + 1, x))
--- CaseStudies/glass/src/Python/NewImplementation/test_All.py
+++ CaseStudies/glass/src/Python/NewImplementation/test_All.py
@@ -1,5 +1,5 @@
 import pytest
-from CurveADT import *
+from FunctADT import *
 from Data import *
 from SeqServices import *
 
@@ -21,12 +21,12 @@ class TestAll:
         X = [1, 2, 10]
         Y = [1, 2, 10]
         o = 1
-        self.c1 = CurveT(X, Y, o)
+        self.c1 = Funct(X, Y, o)
 
         X = [0, 5, 9, 13]
         Y = [0, 3, 30, 27]
         o = 2
-        self.c2 = CurveT(X, Y, o)
+        self.c2 = FuncT(X, Y, o)
 
     def teardown_method(self, method):
         self.c1 = None
@@ -62,32 +62,32 @@ class TestAll:
         assert (index(A, 5.5) == 3) # ensures that x < A[i+1]
         assert (index([1, 2, 3, 4, 5.5], 3.12345678) == 2) # i can be found easily
 
-    # test CurveADT.py
-
+    # test FuncTADT.py
+   
     @staticmethod
-    def test_CurveT_ssmErr():
+    def test_FuncT_ssmErr():
         with pytest.raises(SeqSizeMismatch):
-            c = CurveT([0,1,2], [0], 1)
-            c = CurveT([0], [0,1,2], 2)
+            c = FuncT([0,1,2], [0], 1)
+            c = FuncT([0], [0,1,2], 2)
 
     @staticmethod
-    def test_CurveT_notAscendingErr():
+    def test_FuncT_notAscendingErr():
         with pytest.raises(IndepVarNotAscending):
-            c = CurveT([0,0,0], [0, 1, 3], 1) # x values are equal
-            c = CurveT([5,4,6], [0, 1, 3], 2)
+            c = FuncT([0,0,0], [0, 1, 3], 1) # x values are equal
+            c = FuncT([5,4,6], [0, 1, 3], 2)
 
     @staticmethod
-    def test_CurveT_InvalidInterpErr():
+    def test_FuncT_InvalidInterpErr():
         with pytest.raises(InvalidInterpOrder):
-            c = CurveT([1, 2, 3], [0, 1, 3], 0)
-            c = CurveT([1, 2, 3], [0, 1, 3], 3)
+            c = FuncT([1, 2, 3], [0, 1, 3], 0)
+            c = FuncT([1, 2, 3], [0, 1, 3], 3)
 
-    def test_CurveT_oodErr(self):
+    def test_FuncT_oodErr(self):
         with pytest.raises(OutOfDomain):
             e = self.c1.eval(100)
             e = self.c1.dfdx(0)
             e = self.c2.dfdx(100.607)
-    
+
     def test_minD(self):
         assert (self.c1.minD() == 1)
 
@@ -101,10 +101,3 @@ class TestAll:
         assert (self.c1.eval(3) == 3) # order 1 tested
         assert (self.c2.eval(6) == 7.083333333333334) # order 2 tested
 
-    def test_dfdx(self):
-        assert (self.c1.dfdx(3) == 0.9999999999998899) # order 1 tested
-        assert (self.c2.dfdx(10) == 1.124062500004186) # order 2 tested
-
-    def test_d2fdx2(self):
-        assert (self.c1.d2fdx2(3) == 0) # order 1 tested
-        assert (self.c2.d2fdx2(7) == 1.4999999926601504) # order 2 tested
