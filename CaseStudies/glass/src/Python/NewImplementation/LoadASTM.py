7ea03834d005a015d4b73712b891153a19e8e108
Sam Crawford <sam.crawford82@gmail.com>
2018-07-30 14:07:16 -0400

    Added LoadSDF; finished LoadASTM

--- CaseStudies/glass/src/Python/NewImplementation/LoadASTM.py
+++ CaseStudies/glass/src/Python/NewImplementation/LoadASTM.py
@@ -6,6 +6,8 @@ import pytest
 from ContoursADT import ContoursT
 from FunctADT import FuncT
 
+## @brief Loads cTSD
+#  @param s filename
 def LoadTSD ( s ):
     infile = open(s, "r")
     w_list = infile.readline()
@@ -32,4 +34,30 @@ def LoadTSD ( s ):
         out.add(FuncT(q, SD, 1), w_list[i])
     return out
 
-#def LoadSDF ( s ):
+## @brief Loads cSDF
+#  @param s filename
+def LoadSDF ( s ):
+    infile = open(s, "r")
+    J_list = infile.readline()
+    qStarAndAR = infile.readlines()
+    infile.close()
+
+    J_list = J_list.split()
+    qStar_list, AR_list = [], []
+    for item in qStarAndAR:
+        if item == "\n":
+            qStar_list.append([])
+            AR_list.append([])
+        else:
+            item = item.strip().split()
+            qStar_list[-1].append(item[0])
+            AR_list[-1].append(item[1])
+
+    out = ContoursT(1)
+    for i in range(len(qStar_list)):
+        qStar, AR = [], []
+        for j in range(len(qStar_list[i])):
+            qStar.append(qStar_list[i][j])
+            AR.append(AR_list[i][j])
+        out.add(FuncT(qStar, AR, 1), J_list[i])
+    return out
