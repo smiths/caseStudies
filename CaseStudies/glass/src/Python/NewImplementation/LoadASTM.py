eafeecd36606ece17debcc578f27ca7947e208d1
Sam Crawford <sam.crawford82@gmail.com>
2018-07-31 16:28:30 -0400

    WIP: Reworking LoadASTM to work with input files

--- CaseStudies/glass/src/Python/NewImplementation/LoadASTM.py
+++ CaseStudies/glass/src/Python/NewImplementation/LoadASTM.py
@@ -14,7 +14,10 @@ def LoadTSD ( s ):
     qAndSD = infile.readlines()
     infile.close()
 
-    w_list = w_list.split()
+    w_list = w_list.split(",")[1::2]
+    for i in range(len(w_list)):
+        w_list[i] = w_list[i].strip("\n")
+        w_list[i] = int(w_list[i])
     q_list, SD_list = [], []
     for item in qAndSD:
         if item == "\n":
@@ -42,16 +45,22 @@ def LoadSDF ( s ):
     qStarAndAR = infile.readlines()
     infile.close()
 
-    J_list = J_list.split()
-    qStar_list, AR_list = [], []
+    J_list = J_list.split(",")[1::2]
+    for i in range(len(J_list)):
+        J_list[i] = J_list[i].strip("\n")
+        J_list[i] = int(J_list[i])
+    qStarAndAR = "".join(qStarAndAR).split(",")
+    qStar_list, AR_list = [[]], [[]]
     for item in qStarAndAR:
+        print(item)
+        item = float(item)
+        print(type(item))
         if item == "\n":
             qStar_list.append([])
             AR_list.append([])
         else:
-            item = item.strip().split()
-            qStar_list[-1].append(item[0])
-            AR_list[-1].append(item[1])
+            qStar_list[-1].append(item)
+            AR_list[-1].append(item)
 
     out = ContoursT(1)
     for i in range(len(qStar_list)):
@@ -61,3 +70,5 @@ def LoadSDF ( s ):
             AR.append(AR_list[i][j])
         out.add(FuncT(qStar, AR, 1), J_list[i])
     return out
+
+LoadSDF("SDF.txt")
