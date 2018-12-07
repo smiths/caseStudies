2ffd96b6d54a30caa4a4eed9c03c1a84d004387f
Brooks MacLachlan <brooksel_14@hotmail.com>
2018-12-07 11:26:37 -0500

    Fixed first test and added 2nd

--- CaseStudies/ssp/test/load_params_test.m
+++ CaseStudies/ssp/test/load_params_test.m
@@ -1,18 +1,23 @@
 function tests = load_params_test
-    tests = functiontests(localfunctions)
+    tests = functiontests(localfunctions);
 end
 
 function test_input_slope(testCase)
-    global params_layers
-    expStrat = cell(1,1);
-    expStrat{1} = [0 20 30 40 70; 25 25 20 15 15]
+    global params_layers;
+    expStrat = [0 20 30 40 70; 25 25 20 15 15];
     verifyEqual(testCase, params_layers.strat, expStrat)
+end
+
+function test_input_slope(testCase)
+    global params_layers;
+    expPhi = 0.34906585040;
+    verifyEqual(testCase, params_layers.phi, expPhi, 'RelTol', 1e-10)
 end
 
 function setupOnce(testCase)
     addpath(genpath('dataFiles/'), '../src/');
-    global params_layers, params_piez, params_search, params_soln
-    [params_layers, params_piez, params_search, params_soln] = load_params('ValidInput.txt')
+    global params_layers params_piez params_search params_soln;
+    [params_layers, params_piez, params_search, params_soln] = load_params('ValidInput.txt');
 end
 
 
