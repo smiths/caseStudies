1a133f0a4d2058991030b134be186f3de0c10cc0
Brooks MacLachlan <brooksel_14@hotmail.com>
2018-12-08 11:57:57 -0500

    Added verify_output tests

---
+++ CaseStudies/ssp/test/verify_output_test.m
@@ -0,0 +1,23 @@
+function tests = verify_output_test
+    tests = functiontests(localfunctions);
+end
+
+function test_verify_output_FsNegative(testCase)
+    F = -1;
+    verifyError(testCase, @() verifyError(F), 'verify_output:badFactorOfSafety')
+end
+
+function test_verify_output_Fs0(testCase)
+    F = 0;
+    verifyError(testCase, @() verifyError(F), 'verify_output:badFactorOfSafety')
+end
+
+function test_verify_output_Fs(testCase)
+    F = 1;
+    verify_output(F);
+    verifyEqual(testCase, true, true) %Only passes if verify_output doesn't throw an exception
+end
+
+function setupOnce(testCase)
+    addpath(genpath('dataFiles/'), '../src/');
+end
