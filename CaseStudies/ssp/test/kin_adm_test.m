05f7d3ef45466407ac7a279012ee83c7ed902968
Brooks MacLachlan <brooksel_14@hotmail.com>
2018-12-07 19:27:37 -0500

    Fixed gen_alg test and added more kin_adm tests

--- CaseStudies/ssp/test/kin_adm_test.m
+++ CaseStudies/ssp/test/kin_adm_test.m
@@ -6,6 +6,27 @@ function test_kin_adm_xDec(testCase)
     global strat params_soln;
     slip = [20 30 25 40; 25 15 15 15];
     pass = kin_adm(slip, strat, params_soln);
+    verifyEqual(testCase, pass, false)
+end
+
+function test_kin_adm_xEq(testCase)
+    global strat params_soln;
+    slip = [20 30 30 40; 25 14 14 15];
+    pass = kin_adm(slip, strat, params_soln);
+    verifyEqual(testCase, pass, true)
+end
+
+function test_kin_adm_xEtrOnSlope(testCase)
+    global strat params_soln;
+    slip = [-10 30 35 40; 25 15 12 15];
+    pass = kin_adm(slip, strat, params_soln);
+    verifyEqual(testCase, pass, false)
+end
+
+function test_kin_adm_xEtrOnEdge(testCase)
+    global strat params_soln;
+    slip = [0 30 35 40; 25 15 14 15];
+    pass = kin_adm(slip, strat, params_soln);
     verifyEqual(testCase, pass, true)
 end
 
@@ -14,5 +35,5 @@ function setupOnce(testCase)
     global strat params_soln;
     [params_layer, ~, ~, params_soln] = ...
         load_params('ValidInput.txt');
-    strat = params_layer.strat{1};
+    strat = params_layer.strat;
 end
