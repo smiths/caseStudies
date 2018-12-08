577dd1dff3065b0c59c901dc810c924e8502c16d
Brooks MacLachlan <brooksel_14@hotmail.com>
2018-12-07 20:23:13 -0500

    added another weighter test

--- CaseStudies/ssp/test/weighter_test.m
+++ CaseStudies/ssp/test/weighter_test.m
@@ -9,11 +9,20 @@ function test_weighter_sort(testCase)
     slipsurfs{3, 2} = 1.5;
     newslips = weighter(3, slipsurfs);
     verifyEqual(testCase, newslips{1,2} == 0.8 && newslips{2,2} == 1.0 && ...
-        newslips{3,2}, true)
+        newslips{3,2} == 1.5, true)
+end
+
+function test_weighter_size(testCase)
+    global slipsurfs
+    slipsurfs{1, 2} = 1.0;
+    slipsurfs{2, 2} = 0.8;
+    slipsurfs{3, 2} = 1.5;
+    newslips = weighter(3, slipsurfs);
+    verifyEqual(testCase, length(newslips), 3)
 end
 
 function setupOnce(testCase)
     addpath(genpath('dataFiles/'), '../src/');
     global slipsurfs;
-    slipsurfs = {3, 6};
+    slipsurfs = cell(3, 6);
 end
