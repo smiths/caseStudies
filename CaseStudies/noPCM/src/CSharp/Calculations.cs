cd7e8421593e3312af37bac27e862694e739fb66
Brooks MacLachlan <brooksel_14@hotmail.com>
2020-02-21 16:22:18 -0500

    Update CSharp implementation to more closely reflect what we will use for generated code

--- CaseStudies/noPCM/src/CSharp/Calculations.cs
+++ CaseStudies/noPCM/src/CSharp/Calculations.cs
@@ -4,20 +4,25 @@ using Microsoft.Research.Oslo;
 
 public class Calculations {
     public static List<double> func_T_W(InputParameters inParams) {
-        IEnumerable<SolPoint> sol = Ode.RK547M(
+        Vector f(double t, Vector T_W) {
+          return new Vector((1/inParams.tau_W) * (inParams.T_C - T_W[0]));
+        }
+
+        Options opts = new Options();
+        opts.AbsoluteTolerance = inParams.A_tol;
+        opts.RelativeTolerance = inParams.R_tol;
+
+        IEnumerable<SolPoint> sol = Ode.GearBDF(
             0, 
             new Vector(inParams.T_init), 
-            (t,T_W_ode) => new Vector((1/inParams.tau_W) * (inParams.T_C - T_W_ode[0])),
-            new Options {
-              AbsoluteTolerance = inParams.A_tol,
-              RelativeTolerance = inParams.R_tol
-            });
-        
-        SolPoint[] points = sol.SolveFromToStep(0, inParams.t_final, inParams.t_step).ToArray();
+            f,
+            opts);
+
+        IEnumerable<SolPoint> pts = sol.SolveFromToStep(0, inParams.t_final, inParams.t_step);
 
         List<double> T_W = new List<double>();
-        foreach (SolPoint sp in points) {
-          T_W.Add(sp.X);
+        foreach (SolPoint sp in pts) {
+          T_W.Add(sp.X[0]);
         }
 
         return T_W;
